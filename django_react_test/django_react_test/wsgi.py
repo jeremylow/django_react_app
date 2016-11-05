14e7945e0c3737646ff17dc89a1c1cd3f44123e6
Jeremy Low <jeremylow@gmail.com>
2016-11-05 12:11:05 -0400

    fix lint

--- django_react_test/django_react_test/wsgi.py
+++ django_react_test/django_react_test/wsgi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 WSGI config for django_react_test project.
 
--- django_react_test/posts/models.py
+++ django_react_test/posts/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    published = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
